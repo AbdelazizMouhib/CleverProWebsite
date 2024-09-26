d34040bdd6af57b9edfed65afc7cf2df98d68fc7
none159 <noneenone019@gmail.com>
2024-09-26 22:21:34 +0200

    update

--- src/components/service.js
+++ src/components/service.js
@@ -20,7 +20,7 @@ export const services = [
     name: 'Apps Developpement',
     title: 'Apps Developpement',
     description: 'At CleverPro, app development is approached with a focus on creating custom mobile and web applications that are tailored to meet the unique needs of each client. Their development process begins with a thorough understanding of your business objectives and user requirements, ensuring that the final product is both functional and user-friendly. The UI/UX design team at CleverPro prioritizes intuitive and visually appealing interfaces that enhance user engagement and satisfaction. On the backend, their developers ensure that your applications are secure, scalable, and efficient, providing a robust foundation for your digital solutions. CleverPro also offers ongoing maintenance and support, ensuring that your applications remain up-to-date with the latest features and security enhancements, and continue to perform optimally as your business grows.',
-    image: '/public/icons/smartphone.png',
+    image: '/icons/smartphone.png',
     Moreimage: '/public/assets/appdev.jpeg',
     team: [
       { id: 7, name: 'Abdelaziz Mouhib', position: 'Fondateur et Manager', image: null }, 
@@ -36,8 +36,8 @@ export const services = [
     name: 'Installations',
     title: 'Installations',
     description: 'CleverPro provides a range of installation services aimed at improving energy efficiency, connectivity, and security for businesses and homes. Their solar panel installation services include everything from initial consultation and design to installation and maintenance, helping you reduce energy costs and contribute to a sustainable future. For networking solutions, CleverPro offers comprehensive services that cover the setup of both wired and wireless networks, ensuring reliable and high-speed connectivity that supports your business operations. When it comes to security, CleverPro installs state-of-the-art CCTV systems tailored to your specific needs. Their range of cameras and monitoring solutions provide enhanced security and peace of mind, allowing you to monitor your premises effectively and deter potential threats.',
-    image: '/public/icons/maintenance.png',
-    Moreimage: '/public/assets/installations.jpeg',
+    image: '/icons/maintenance.png',
+    Moreimage: '/assets/installations.jpeg',
     team: [
       { id: 13, name: 'Khalil Rossaissy', position: "Chef d'équipe", image: null }, 
       { id: 14, name: 'Chems eddin Aberchih', position: 'Technicienne', image: null },
@@ -50,8 +50,8 @@ export const services = [
     name: 'CO-OP',
     title: 'CO-OP',
     description: 'CleverPro’s co-op services are designed to foster collaboration, innovation, and personal growth. Their modern coworking spaces provide a flexible and dynamic environment where professionals and entrepreneurs can work, network, and share ideas. These spaces are equipped with high-speed internet, meeting rooms, and all the amenities needed to support productivity and creativity. In addition to coworking spaces, CleverPro offers coaching services that provide personalized guidance and support to help individuals and businesses achieve their goals. Their experienced coaches work with you to identify your strengths and areas for improvement, develop actionable plans, and provide the motivation and accountability needed to succeed. Whether you’re looking to grow your business, enhance your skills, or navigate a career transition, CleverPro’s coaching services are designed to help you reach your full potential.',
-    image: '/public/icons/mentor.png',
-    Moreimage: '/public/assets/coop.jpeg',
+    image: '/icons/mentor.png',
+    Moreimage: '/assets/coop.jpeg',
     team: [
       { id: 17, name: 'Abdelaziz Mouhib', position: 'Responsable', image: null }
     ]
